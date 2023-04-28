7edb5a715af8332a6d7c125ec55c9e86910f7dee
Anson Hunnell <ansonhunnell@gmail.com>
2023-04-28 08:52:20 -0400

    send affiliateStat info to mongoDB

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
     /* ONLY ADD DATA ONE TIME */
-    // AffiliateStat.insertMany(dataAffiliateStat);
+    // AffiliateStat.insertMany(dataAffiliateStat)
     // OverallStat.insertMany(dataOverallStat)
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat)
